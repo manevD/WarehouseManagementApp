437359aa745b65c568755a25f99f7d2423ce3503
Dimitar Manev <dimitar.manev@outlook.de>
2026-09-08 22:28:52 +0200

    HomePage

---
+++ Components/Pages/Index.razor.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseManagement.Components.Pages;
+
+public partial class Index
+{
+    protected bool MenuOpen { get; set; }
+
+    protected bool Submitted { get; set; }
+
+    protected DemoRequest DemoRequestItem { get; set; } = new();
+
+    protected void ToggleMenu()
+    {
+        MenuOpen = !MenuOpen;
+    }
+
+    protected void CloseMenu()
+    {
+        MenuOpen = false;
+    }
+
+    protected void SubmitDemo()
+    {
+        Submitted = true;
+    }
+
+    public sealed class DemoRequest
+    {
+        [Required(ErrorMessage = "Bitte Namen eingeben.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Bitte Unternehmen eingeben.")]
+        public string Company { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Bitte E-Mail eingeben.")]
+        [EmailAddress(ErrorMessage = "Bitte gültige E-Mail eingeben.")]
+        public string Email { get; set; } = string.Empty;
+
+        public string Industry { get; set; } = "Großhandel";
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
